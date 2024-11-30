7b582d8df5d30638b4e9d26c985ddebff18f3d23
Tomas Pavlatka <tomas.pavlatka@zolar.de>
2024-11-30 14:53:17 +0100

    Adding recent files plugin

--- lua/tomaspavlatka/plugins/lsp.lua
+++ lua/tomaspavlatka/plugins/lsp.lua
@@ -1,96 +1,121 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "j-hui/fidget.nvim",
-    },
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "saadparwaiz1/cmp_luasnip",
+    "j-hui/fidget.nvim",
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+      build = "make install_jsregexp"
+    }
+  },
 
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+  config = function()
+    local cmp = require('cmp')
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities())
 
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "gopls",
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
+    require("fidget").setup({})
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "rust_analyzer",
+      },
+      handlers = {
+        function(server_name) -- default handler (optional)
+          require("lspconfig")[server_name].setup {
+            capabilities = capabilities
+          }
+        end,
+      }
+    })
 
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-            }
-        })
+    -- I could not manage to load the snippets from another file, so as ugly as it might be
+    -- they all will leave here for the time being
+    local ls = require("luasnip")
+    local lss = ls.snippet
+    local lst = ls.text_node
+    local lsi = ls.insert_node
+    local lsrep = require("luasnip.extras").rep
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+    ls.add_snippets("typescript", {
+      lss("tecommand", {
+        lst({"import { ContextAwareException } from '@common/exceptions/context-aware.exception';"}),
+        lst({"", "import { Injectable } from '@nestjs/common';"}),
+        lst({"", "import { Either } from '@common/either';"}),
+        lst({"", "", "@Injectable()",  "export class " }),
+        lsi(1, "Command"),
+        lst({ " {", "  constructor() {}" }),
+        lst({ "", "  async execute(): Promise<Either<ContextAwareException, >> {", "  }" }),
+        lst({ "", "}" }),
+      }),
 
-        cmp.setup({
-          snippet = {
-            expand = function(args)
-              require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            end,
-          },
-          mapping = cmp.mapping.preset.insert({
-            ['<C-i>'] = cmp.mapping.select_prev_item(cmp_select),
-            ['<C-u>'] = cmp.mapping.select_next_item(cmp_select),
-            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-            ["<C-Space>"] = cmp.mapping.complete(),
-          }),
-          sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
-            { name = "copilot" },
+      lss("tedto", {
+        lst({ "import { plainToInstance } from 'class-transformer';", "", "export class " }),
+        lsi(1, "Dto"),
+        lst({ " {", "  static create(data: Required<" }),
+        lsrep(1),
+        lst( {">) {", "    return plainToInstance(" }),
+        lsrep(1),
+        lst( {", data);"}),
+        lst( {"", "  }", "}"}),
+      }),
 
-          }, {
-              { name = 'buffer' },
-            })
-        })
+      lss("tenotimplementedexception", {
+        lst({ "return Either.left(NotImplementedException.create());" }),
+      }),
+    });
 
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-        local opts = {buffer = false}
-        vim.keymap.set('n', '<leader>gr', '<cmd>Telescope lsp_references<cr>', opts)
-        vim.keymap.set('n', '<leader>gd', '<cmd>Telescope lsp_definitions<cr>', opts)
-        vim.keymap.set('n', '<leader>gi', '<cmd>Telescope lsp_implementations<cr>', opts)
-        vim.keymap.set('n', '<leader>dl', '<cmd>Telescope diagnostics<cr>')
-    end
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-i>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-u>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+
+      sources = cmp.config.sources({
+        { name = 'luasnip' },
+        { name = 'nvim_lsp' },
+      })
+    })
+
+
+    vim.diagnostic.config({
+      -- update_in_insert = true,
+      float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
+
+    local opts = { buffer = false }
+    vim.keymap.set('n', '<leader>gr', '<cmd>Telescope lsp_references<cr>', opts)
+    vim.keymap.set('n', '<leader>gd', '<cmd>Telescope lsp_definitions<cr>', opts)
+    vim.keymap.set('n', '<leader>gi', '<cmd>Telescope lsp_implementations<cr>', opts)
+    vim.keymap.set('n', '<leader>dl', '<cmd>Telescope diagnostics<cr>')
+  end
 }
--- lua/tomaspavlatka/plugins/telescope.lua
+++ lua/tomaspavlatka/plugins/telescope.lua
@@ -4,11 +4,21 @@ return {
   tag = "0.1.5",
 
   dependencies = {
-    "nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim",
+    "smartpde/telescope-recent-files",
   },
 
   config = function()
     require('telescope').setup({
+      defaults = {
+        prompt_prefix = "🔍 ", -- Customize the prompt
+        selection_caret = " ",
+        path_display = { "truncate" }, -- Display file paths in truncated form
+        sorting_strategy = "ascending",
+      },
+      extensions = {
+        recent_files = {}
+      },
       pickers = {
         git_files = {
           theme = "dropdown",
@@ -19,19 +29,20 @@ return {
       }
     })
 
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>pa', builtin.git_files, {})
+    local b = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>pf', b.git_files, {})
+    vim.keymap.set('n', '<leader>ff', b.find_files, {})
+    vim.keymap.set('n', '<leader>lf', ':lua require("telescope").extensions.recent_files.pick()<cr>', {})
     vim.keymap.set('n', '<leader>pws', function()
       local word = vim.fn.expand("<cword>")
-      builtin.grep_string({ search = word })
+      b.grep_string({ search = word })
     end)
     vim.keymap.set('n', '<leader>pWs', function()
       local word = vim.fn.expand("<cWORD>")
-      builtin.grep_string({ search = word })
+      b.grep_string({ search = word })
     end)
     vim.keymap.set('n', '<leader>ps', function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+      b.grep_string({ search = vim.fn.input("Grep > ") })
     end)
   end
 }
